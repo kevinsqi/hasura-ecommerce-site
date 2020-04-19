4403e24e623422847029b8b74621521f05806d0c
Kevin Qi <iqnivek@gmail.com>
2020-04-18 17:03:36 -0700

    get wishlists gql query working

--- src/App.js
+++ src/App.js
@@ -1,24 +1,39 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 
+import gql from 'graphql-tag';
+import { useQuery } from '@apollo/react-hooks';
+
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+  const { loading, error, data } = useQuery(gql`
+    query getWishlists {
+      users(where: {id: {_eq: 1}}) {
+        id
+        email
+        wishlists {
+          id
+          name
+          wishlist_products {
+            product {
+              id
+              name
+              price
+            }
+          }
+        }
+      }
+    }
+  `);
+
+  if (error) {
+    return <div>An error occurred</div>;
+  }
+
+  return loading ? (
+    <div>Loading...</div>
+  ) : (
+    <div>
+      {JSON.stringify(data)}
     </div>
   );
 }
